d7cb086a76b8591d6615846c33159e283abe73c1
Tamal Kundu <166101607+tkunduHitachi@users.noreply.github.com>
2024-07-30 18:03:09 +0530

    test

--- terraformprovider.tf
+++ terraformprovider.tf
@@ -1,3 +1,4 @@
+# Provider to be downloaded from terraform hitachi
 terraform {
   required_providers {
     hitachi = {
@@ -14,6 +15,15 @@ provider "hitachi" {
     username          = "admin"
     password          = "Hitachi2!"
   }
+}
+
+# Retrieve all iSCSI CHAP users
+data "hitachi_vss_block_iscsi_chap_users" "my_chap_users" {
+  vss_block_address = "10.10.12.13"
+}
+
+output "my_iscsi_chap_users_output" {
+  value = data.hitachi_vss_block_iscsi_chap_users.my_chap_users
 }
 
 # Create: chap user.
